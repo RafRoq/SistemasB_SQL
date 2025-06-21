e0b3522058b85d61f859e00c034bfcbc4fb5474f
Rafael <rafael.roquec@gmail.com>
2025-06-21 18:20:16 -0300

    testestes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from database.database import get_database_connection
 
 def main():
     database = get_database_connection()
+    database.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)")
 
 if __name__ == "__main__":
     main()
