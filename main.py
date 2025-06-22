9e62b0562573010999c2541db587b70efbc67412
Rafael <rafael.roquec@gmail.com>
2025-06-22 20:09:52 -0300

    asdasda

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from database.database import DataBase
 import pandas as pd
 
 def main():
-    db_manager = DataBase(initialize=False, create_views=True)
-    teste = pd.read_sql_query(db_manager.query_from_file(r'queries\DQL\desempenhos_tenicos.sql'), db_manager.conn)
+    db_manager = DataBase(initialize=False, create_views=False)
+    teste = db_manager.to_dataframe(db_manager.query_from_file(r'queries\DQL\desempenhos_tenicos.sql'))
     print(teste)
 if __name__ == "__main__":
     main()
